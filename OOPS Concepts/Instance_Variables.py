b2b4cdbca5ba10c7323b26959a910a98f4f09da1
raka2407 <u.rakesh@live.com>
2020-04-29 17:56:04 +0530

    Instance Variables

--- OOPS Concepts/Instance_Variables.py
+++ OOPS Concepts/Instance_Variables.py
@@ -5,11 +5,15 @@ class ExampleClass:
     def setSecond(self, val):
         self.__second = val
 
-eobject1 = ExampleClass(10)
-eobject1.setSecond(20)
+eobject1 = ExampleClass(10)  # eobject1 has only 1 property named first
 
 eobject2 = ExampleClass()
-eobject2.setSecond(2)
+eobject2.setSecond(2)        # eobject2 has 2 properties: named first and second
+
+eobject3 = ExampleClass(20)
+eobject3.setSecond(30)
+eobject3.third = 30          # eobject3 has 3 properties: named first, second and third. 
 
 print(eobject1.__dict__)
 print(eobject2.__dict__)
+print(eobject3.__dict__)
