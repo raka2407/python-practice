803ac10cdbeed0a88461b4201b7c17e03ae00d39
raka2407 <u.rakesh@live.com>
2020-05-01 13:43:06 +0530

    Multiple Inheritance - Imp Notes

--- OOPS Concepts/Multiple _Inheritance.py
+++ OOPS Concepts/Multiple _Inheritance.py
@@ -17,3 +17,9 @@ obj = C()
 print(obj.varA, obj.funA(), sep="\t")
 print(obj.varB, obj.funB(), sep="\t")
 print(obj.varC, obj.funC(), sep="\t")
+
+# Important Notes
+
+# 1. If Class B and C have the same variable name (for ex: var), the value defined in Class B is overridden by class C
+# 2. Python uses "Bottom Up Approach" to read the entities.
+# 3, When 2 superclasses have the same names for entities and if python cannot deduce the inherance path: the precedence is taken from Left --> Right (In the example above: First class 'A' followed by 'B') 
